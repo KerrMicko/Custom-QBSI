cbb8230dbe14a8cba4f6f1bb2bfd60b729f12c98
KerrMicko <mixy7777@gmail.com>
2025-07-15 13:39:12 +0800

    Add project files.

---
+++ Custom-QBSI/Dashboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Custom_QBSI
+{
+    public class GlobalVariables
+    {
+        public static string client = "NHC";
+    }
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+
+            this.WindowState = FormWindowState.Maximized;
+            this.Text = "QBSI " + GlobalVariables.client;
+        }
+    }
+}
