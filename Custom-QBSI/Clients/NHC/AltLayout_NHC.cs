e59754c685e992e7a618f7cb70d649f9f63afe9b
Itsmeeedanhil <djampatin@gmail.com>
2025-09-16 10:40:46 +0800

    ADJUSTMENT LAYOUT FOR NULLS

--- Custom-QBSI/Clients/NHC/AltLayout_NHC.cs
+++ Custom-QBSI/Clients/NHC/AltLayout_NHC.cs
@@ -36,8 +36,8 @@ namespace Custom_QBSI.Clients.NHC
             public void Layout_SalesInvoice(PrintPageEventArgs e, List<InvoiceData> invoiceData, string vatType, string businessStyle, string signatoryName, bool isLessEWTChecked)
             {
 
-                Image image = Properties.Resources.NATURE_SI;
-                e.Graphics.DrawImage(image, e.PageBounds);
+               /* Image image = Properties.Resources.NATURE_SI;
+                e.Graphics.DrawImage(image, e.PageBounds);*/
 
                 Font font_Data = font_Eight;
 
@@ -439,8 +439,8 @@ namespace Custom_QBSI.Clients.NHC
 
             public void Layout_DeliveryReceipt(PrintPageEventArgs e, List<InvoiceData> invoiceData, string note, string businessStyle, string pwdSignature, bool isEnableExpDateChecked, string signatoryName)
             {
-                Image image = Properties.Resources.NATURE_DR;
-                e.Graphics.DrawImage(image, e.PageBounds);
+                /*Image image = Properties.Resources.NATURE_DR;
+                e.Graphics.DrawImage(image, e.PageBounds);*/
 
                 Font font_Data = font_Eight;
 
